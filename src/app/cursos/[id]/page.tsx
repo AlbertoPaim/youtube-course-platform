4175d23ab7664eb17d9b9169573f84ddfcb27b09
Alberto Paim <albertoopaim@gmail.com>
2025-06-01 10:14:45 -0300

    feat:start course floating desktop settings

--- src/app/cursos/[id]/page.tsx
+++ src/app/cursos/[id]/page.tsx
@@ -15,16 +15,32 @@ interface Props {
 export default function CourseDetail({ }: Props) {
     return (
         <main className="mt-2 flex justify-center">
-            <div className="w-full min-[880px]:max-w-[880px] px-2 flex flex-col gap-4">
-                <StartCourse
-                    idClass=""
-                    idCourse=""
-                    imageUrl="https://i.ytimg.com/vi/bP47qRVRqQs/hqdefault.jpg"
-                />
-                <CourseHeader />
-                <CourseHeader />
-                <CourseHeader />
-                <CourseHeader />
+            <div className="w-full min-[880px]:max-w-[880px] px-2 flex flex-col gap-4 md:flex-row-reverse">
+
+                <div className="flex-1">
+                    <StartCourse
+                        idClass=""
+                        idCourse=""
+                        imageUrl="https://i.ytimg.com/vi/bP47qRVRqQs/hqdefault.jpg"
+                    />
+                </div>
+
+                <div className="flex-[2]">
+                    <CourseHeader />
+                    <CourseHeader />
+                    <CourseHeader />
+                    <CourseHeader />
+                    <CourseHeader />
+                    <CourseHeader />
+                    <CourseHeader />
+                    <CourseHeader />
+                    <CourseHeader />
+                    <CourseHeader />
+                    <CourseHeader />
+                    <CourseHeader />
+                    <CourseHeader />
+
+                </div>
             </div>
         </main>
     );
--- src/components/StartCourse.tsx
+++ src/components/StartCourse.tsx
@@ -16,29 +16,29 @@ export const StartCourse = ({ idClass, idCourse, imageUrl }: ICouseStartProps) =
 
     return (
         <>
-            <div ref={ref} className="bg-paper p-3 rounded-md flex flex-col gap-4">
+            <div ref={ref} className="bg-paper p-3 rounded-md flex flex-col gap-4 md:sticky md:top-16">
                 <Link href={`/player/${idCourse}/${idClass}`}
                     style={{ backgroundImage: `url(${imageUrl})` }}
-                    className="w-full bg-cover bg-no-repeat aspect-video bg-center rounded-3xl hover:no-underline">
+                    className="w-full bg-cover bg-no-repeat aspect-video bg-center rounded-3xl md:rounded-sm hover:no-underline">
 
-                    <div className="w-full h-full flex items-center justify-center bg-black opacity-0 hover:opacity-50 rounded transition-opacity  rounded-3xl ">
+                    <div className="w-full h-full flex items-center justify-center bg-black opacity-0 hover:opacity-50 transition-opacity  rounded-3xl md:rounded-sm">
                         <MdPlayCircleOutline
                             size={60} />
                     </div>
                 </Link>
 
                 <Link href={`/player/${idCourse}/${idClass}`}
-                    className="bg-primary px-2 rounded-md text-center hover:no-underline py-1 text-lg font-semibold">
+                    className="bg-primary px-2 rounded-md text-center hover:no-underline py-1 text-lg font-semibold hover:opacity-80">
                     Começar curso
                 </Link>
             </div>
 
             {
                 !inView && (
-                    <div className="bg-paper p-3 px-2 rounded-md flex flex-col gap-4 absolute left-0 right-0 top-14">
+                    <div className="bg-paper p-3 px-2 rounded-md flex flex-col gap-4 absolute left-0 right-0 top-14 ">
                         <h1 className="font-extrabold text-xl">Title</h1>
                         <Link href={`/player/${idCourse}/${idClass}`}
-                            className="bg-primary px-2 rounded-md text-center hover:no-underline py-1 text-lg font-semibold">
+                            className="bg-primary px-2 rounded-md text-center hover:no-underline py-1 text-lg font-semibold hover:opacity-80">
                             Começar curso
                         </Link>
                     </div>
